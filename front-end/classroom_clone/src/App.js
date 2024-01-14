c8d674f6bf1cd2b725ff22d72855c2b3197f3c6c
dev-rabin <robinmandhotia@gmail.com>
2024-01-14 21:03:29 +0530

    frontend-setup

---
+++ front-end/classroom_clone/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import Homepage  from "./components/homepage.js";
+import NavbarPage from './components/navbar.js';
+
+function App() {
+  return(
+    NavbarPage(),
+    Homepage()) ;
+}
+export default App;
